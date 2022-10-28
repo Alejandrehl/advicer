2d5a83ecaa80adeb47ed0f669ca6df5630ebabde
Alejandro Hernández <alejandrehl@icloud.com>
2022-10-28 11:29:49 -0300

    Avances

--- lib/application/pages/advice/bloc/advicer_state.dart
+++ lib/application/pages/advice/bloc/advicer_state.dart
@@ -4,3 +4,21 @@ part of 'advicer_bloc.dart';
 abstract class AdvicerState {}
 
 class AdvicerInitial extends AdvicerState {}
+
+class AdvicerStateLoading extends AdvicerState {}
+
+class AdvicerStateLoaded extends AdvicerState {
+  final String advice;
+
+  AdvicerStateLoaded({
+    required this.advice,
+  });
+}
+
+class AdvicerStateError extends AdvicerState {
+  final String message;
+
+  AdvicerStateError({
+    required this.message,
+  });
+}
