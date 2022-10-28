490dfcd22e90ab061c0e47910605be0132a2502b
Alejandro Hernández <alejandrehl@icloud.com>
2022-10-27 21:29:43 -0300

    Application theme

--- lib/theme.dart
+++ lib/theme.dart
@@ -3,25 +3,95 @@ import 'package:flutter/material.dart';
 class AppTheme {
   AppTheme._();
 
-  static const _primaryColorLight = Colors.lightBlueAccent;
-  static const _primaryColorDark = Colors.lightGreenAccent;
+  // *****************
+  // static colors
+  // *****************
+  static final Color _lightPrimaryColor = Colors.blueGrey.shade50;
+  static final Color _lightPrimaryVariantColor = Colors.blueGrey.shade800;
+  static final Color _lightOnPrimaryColor = Colors.blueGrey.shade200;
+  static const Color _lightTextColorPrimary = Colors.black;
+  static const Color _appbarColorLight = Colors.blue;
+
+  static final Color _darkPrimaryColor = Colors.blueGrey.shade900;
+  static const Color _darkPrimaryVariantColor = Colors.black;
+  static final Color _darkOnPrimaryColor = Colors.blueGrey.shade300;
+  static const Color _darkTextColorPrimary = Colors.white;
+  static final Color _appbarColorDark = Colors.blueGrey.shade800;
+
+  static const Color _iconColor = Colors.white;
+
+  static const Color _accentColor = Color.fromRGBO(74, 217, 217, 1);
+
+  // *****************
+  // Text Style - light
+  // *****************
+  static const TextStyle _lightHeadingText = TextStyle(
+    color: _lightTextColorPrimary,
+    fontFamily: "Rubik",
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle _lightBodyText = TextStyle(
+    color: _lightTextColorPrimary,
+    fontFamily: "Rubik",
+    fontStyle: FontStyle.italic,
+    fontWeight: FontWeight.bold,
+    fontSize: 16,
+  );
+
+  static const TextTheme _lightTextTheme = TextTheme(
+    headline1: _lightHeadingText,
+    bodyText1: _lightBodyText,
+  );
+
+  // *****************
+  // Text Style - dark
+  // *****************
+  static final TextStyle _darkThemeHeadingTextStyle =
+      _lightHeadingText.copyWith(color: _darkTextColorPrimary);
+
+  static final TextStyle _darkThemeBodyeTextStyle =
+      _lightBodyText.copyWith(color: _darkTextColorPrimary);
+
+  static final TextTheme _darkTextTheme = TextTheme(
+    headline1: _darkThemeHeadingTextStyle,
+    bodyText1: _darkThemeBodyeTextStyle,
+  );
+
+  // *****************
+  // Theme light/dark
+  // *****************
 
   static final ThemeData lightTheme = ThemeData(
-    colorScheme: ColorScheme.fromSeed(
-      seedColor: _primaryColorLight,
-      brightness: Brightness.light,
+    scaffoldBackgroundColor: _lightPrimaryColor,
+    appBarTheme: const AppBarTheme(
+      color: _appbarColorLight,
+      iconTheme: IconThemeData(color: _iconColor),
     ),
-    useMaterial3: true,
+    bottomAppBarColor: _appbarColorLight,
+    colorScheme: ColorScheme.light(
+      primary: _lightPrimaryColor,
+      onPrimary: _lightOnPrimaryColor,
+      secondary: _accentColor,
+      primaryContainer: _lightPrimaryVariantColor,
+    ),
+    textTheme: _lightTextTheme,
   );
 
   static final ThemeData darkTheme = ThemeData(
-    useMaterial3: true,
-    appBarTheme: const AppBarTheme(
-      centerTitle: false,
+    scaffoldBackgroundColor: _darkPrimaryColor,
+    appBarTheme: AppBarTheme(
+      color: _appbarColorDark,
+      iconTheme: const IconThemeData(color: _iconColor),
     ),
-    colorScheme: ColorScheme.fromSeed(
-      seedColor: _primaryColorDark,
-      brightness: Brightness.dark,
+    bottomAppBarColor: _appbarColorDark,
+    colorScheme: ColorScheme.dark(
+      primary: _darkPrimaryColor,
+      secondary: _accentColor,
+      onPrimary: _darkOnPrimaryColor,
+      primaryContainer: _darkPrimaryVariantColor,
     ),
+    textTheme: _darkTextTheme,
   );
 }
