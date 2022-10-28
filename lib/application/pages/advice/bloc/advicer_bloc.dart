000421439c0662882bda83ec3e576feb120706c0
Alejandro Hernández <alejandrehl@icloud.com>
2022-10-28 11:05:53 -0300

    Base de mi primer bloc

---
+++ lib/application/pages/advice/bloc/advicer_bloc.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'advicer_event.dart';
+part 'advicer_state.dart';
+
+class AdvicerBloc extends Bloc<AdvicerEvent, AdvicerState> {
+  AdvicerBloc() : super(AdvicerInitial()) {
+    on<AdvicerEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
