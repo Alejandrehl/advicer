78285d23bbd959076d08f5447d80d6c755dee70b
Alejandro Hernández <alejandrehl@icloud.com>
2022-10-30 12:35:07 -0300

    Handling failures like a pro

--- lib/application/pages/advice/bloc/advicer_bloc.dart
+++ lib/application/pages/advice/bloc/advicer_bloc.dart
@@ -2,6 +2,7 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../domain/failures/failures.dart';
 import '../../../../domain/usecases/advice_usecases.dart';
 
 part 'advicer_event.dart';
@@ -17,9 +18,22 @@ class AdvicerBloc extends Bloc<AdvicerEvent, AdvicerState> {
       final failureOrAdvice = await adviceUseCases.getAdvice();
 
       failureOrAdvice.fold(
-        (failure) => emit(AdvicerStateError(message: 'Error message')),
+        (failure) => emit(AdvicerStateError(
+          message: _mapFailureToMessage(failure),
+        )),
         (advice) => emit(AdvicerStateLoaded(advice: advice.advice)),
       );
     });
+  }
+
+  String _mapFailureToMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return 'Ups, API Error, please try again';
+      case CacheFailure:
+        return 'Ups, cache failed. Please try again.';
+      default:
+        return 'Ups, something gone wrong. Please try again.';
+    }
   }
 }
--- lib/application/pages/advice/cubit/advicer_cubit.dart
+++ lib/application/pages/advice/cubit/advicer_cubit.dart
@@ -2,6 +2,7 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../domain/failures/failures.dart';
 import '../../../../domain/usecases/advice_usecases.dart';
 
 part 'advicer_state.dart';
@@ -16,8 +17,21 @@ class AdvicerCubit extends Cubit<AdvicerCubitState> {
     final failureOrAdvice = await adviceUseCases.getAdvice();
 
     failureOrAdvice.fold(
-      (failure) => emit(AdvicerStateError(message: 'Error message')),
+      (failure) => emit(AdvicerStateError(
+        message: _mapFailureToMessage(failure),
+      )),
       (advice) => emit(AdvicerStateLoaded(advice: advice.advice)),
     );
+  }
+
+  String _mapFailureToMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return 'Ups, API Error, please try again';
+      case CacheFailure:
+        return 'Ups, cache failed. Please try again.';
+      default:
+        return 'Ups, something gone wrong. Please try again.';
+    }
   }
 }
