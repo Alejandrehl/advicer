9b6e0d8660df2d7895c96b9ef3f033f4e9dd39f2
Alejandro Hernández <alejandrehl@icloud.com>
2022-10-29 23:15:46 -0300

    Save

--- lib/application/pages/advice/advice_page.dart
+++ lib/application/pages/advice/advice_page.dart
@@ -1,8 +1,23 @@
 import 'package:advicer/application/core/services/theme_service.dart';
+import 'package:advicer/application/pages/advice/bloc/advicer_bloc.dart';
 import 'package:advicer/application/pages/advice/widgets/advice_field.dart';
 import 'package:advicer/application/pages/advice/widgets/custom_button.dart';
+import 'package:advicer/application/pages/advice/widgets/error_message.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
+
+class AdvicerPageWrapperProvider extends StatelessWidget {
+  const AdvicerPageWrapperProvider({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => AdvicerBloc(),
+      child: const AdvicePage(),
+    );
+  }
+}
 
 class AdvicePage extends StatelessWidget {
   const AdvicePage({Key? key}) : super(key: key);
@@ -31,16 +46,43 @@ class AdvicePage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 50.0),
         child: Column(
-          children: const [
+          children: [
             Expanded(
               child: Center(
-                child: AdviceField(advice: 'Your advice is waiting for you!'),
+                child: BlocBuilder<AdvicerBloc, AdvicerState>(
+                  builder: (context, state) {
+                    if (state is AdvicerInitial) {
+                      return Text(
+                        'Your advice is waiting for you!',
+                        style: themeData.textTheme.headline1,
+                        textAlign: TextAlign.center,
+                      );
+                    } else if (state is AdvicerStateLoading) {
+                      return CircularProgressIndicator(
+                        color: themeData.colorScheme.secondary,
+                      );
+                    } else if (state is AdvicerStateLoaded) {
+                      return AdviceField(
+                        advice: state.advice,
+                      );
+                    } else if (state is AdvicerStateError) {
+                      return ErrorMessage(message: state.message);
+                    }
+
+                    return const SizedBox();
+                  },
+                ),
               ),
             ),
             SizedBox(
               height: 200,
               child: Center(
-                child: CustomButton(),
+                child: CustomButton(
+                  title: 'Get Advice',
+                  onPressed: () => BlocProvider.of<AdvicerBloc>(context).add(
+                    AdviceRequestEvent(),
+                  ),
+                ),
               ),
             ),
           ],
--- lib/application/pages/advice/widgets/custom_button.dart
+++ lib/application/pages/advice/widgets/custom_button.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({Key? key}) : super(key: key);
+  final String title;
+  final Function onPressed;
+
+  const CustomButton({
+    Key? key,
+    required this.title,
+    required this.onPressed,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final themeData = Theme.of(context);
 
     return InkResponse(
-      onTap: () => print('Button pressed - will be implemented later'),
+      onTap: () => onPressed(),
       child: Material(
         elevation: 20.0,
         borderRadius: BorderRadius.circular(15.0),
@@ -23,7 +30,7 @@ class CustomButton extends StatelessWidget {
               vertical: 15.0,
             ),
             child: Text(
-              'Get Advice',
+              title,
               style: themeData.textTheme.headline1,
             ),
           ),
